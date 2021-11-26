d9f63e89307b5b2d1460c3aaa04603520d313051
musadevw <musadevweb@gmail.com>
2021-11-26 20:09:55 +0000

    first

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+// const path = require('path')
+// const config = require('config')
+// const sequelize = require('./utils/config/db.config')
+const app = express()
+
+// sequelize.sync({force:true}).then(()=>{
+//     console.log('connecter')
+// })
+app.use(express.json())
+app.use(express.urlencoded({
+    extended:true
+}))
+
+// if(process.env.NODE_ENV === 'production'){
+//     app.use(express.static('client/build'))
+//     app.get('*', (req,res)=>{
+//         res.sendFile(path.resolve(__dirname,'client','build','index.'))
+//     })
+// }
+
+const PORT = process.env.PORT
+
+app.listen(PORT,()=>console.log(`connecté avec succes sur ${PORT}`))
